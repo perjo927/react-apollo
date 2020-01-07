dd409230d9f649001d818c010850e41bfb78fc3a
perjo927 <djpjgj@gmail.com>
2020-01-07 11:50:20 +0100

    init: add mutations

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -39,12 +39,12 @@ class CreateLink extends Component {
                         placeholder="The URL for the link"
                     />
                 </div>
-                <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
-                    {() => (
-                        <button onClick={`... you'll implement this 🔜`}>
-                            Submit
-                        </button>
-                    )}
+                <Mutation 
+                    mutation={POST_MUTATION} 
+                    variables={{ description, url }}
+                    onCompleted={() => this.props.history.push('/')
+                }>
+                    {postMutation => <button onClick={postMutation}>Submit</button>}
                 </Mutation>
             </div>
         )
